e3af4b2410f877533ed7baac7a3724cc74995d44
LeivaDiego <lei21752@uvg.edu.gt>
2023-05-29 05:11:06 -0600

    Clase Seguridad
    Esta clase tiene los metodos necesarios para encriptar y desencriptar informacion del usuario con el algoritmo del cesar, asimismo, hay un metodo que sirve para verificar si una contrasñea es menor a 8 caracteres

---
+++ Proyecto Version Consola/GuiaOtakuConsole/src/Security.java
@@ -0,0 +1,44 @@
+public class Security {
+    private static final int SHIFT = 3; // cantidad de desplazamiento para el cifrado de César
+
+    public static boolean isLessThanEightChars(String str) {
+        return str.length() < 8;
+    }
+    public static String encrypt(String plaintext) {
+        StringBuilder cipherText = new StringBuilder();
+
+        for (char c : plaintext.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char encryptedChar = (char) ((c + SHIFT - 'A') % 26 + 'A');
+                cipherText.append(encryptedChar);
+            } else if (Character.isLowerCase(c)) {
+                char encryptedChar = (char) ((c + SHIFT - 'a') % 26 + 'a');
+                cipherText.append(encryptedChar);
+            } else {
+                cipherText.append(c); // no alterar otros caracteres
+            }
+        }
+
+        return cipherText.toString();
+    }
+
+    public static String decrypt(String cipherText) {
+        StringBuilder plaintext = new StringBuilder();
+
+        for (char c : cipherText.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char decryptedChar = (char) ((c - SHIFT - 'A' + 26) % 26 + 'A');
+                plaintext.append(decryptedChar);
+            } else if (Character.isLowerCase(c)) {
+                char decryptedChar = (char) ((c - SHIFT - 'a' + 26) % 26 + 'a');
+                plaintext.append(decryptedChar);
+            } else {
+                plaintext.append(c); // no alterar otros caracteres
+            }
+        }
+
+        return plaintext.toString();
+    }
+
+
+}
